fb71e8f5f63d5184f9bdb916e90f8a87cd518eb1
Gerardo Leal <gerard.leal91@gmail.com>
2016-10-17 17:18:16 -0300

    2.- The Hero Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Study Angular 2 - Tour of Heroes';
+
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  }
+
 }
