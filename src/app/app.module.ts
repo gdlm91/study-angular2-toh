1de8e115a508a4326c678a26a0e6e5951844c7f0
Gerardo Leal <gerard.leal91@gmail.com>
2016-10-19 17:46:43 -0300

    6.1 Routing - Dashboard List (no detail route)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { MaterializeModule } from "angular2-materialize";
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component'
+
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ]),
     FormsModule,
     HttpModule,
     MaterializeModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Hero } from './hero';
 
 @Component({
-  selector: 'my-hero-detail',
+  selector: 'hero-detail',
   template: `
     <!-- Details -->
     <div *ngIf="hero">
